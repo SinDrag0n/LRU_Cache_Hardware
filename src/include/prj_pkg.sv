98c8f4583f9b53b3a9ce1178850e359a3a84dae3
SinDrag0n <vladislavbelyaev04@gmail.com>
2025-07-07 20:07:52 +0300

    add (includes): moved parameters from module file to package

--- src/include/prj_pkg.sv
+++ src/include/prj_pkg.sv
@@ -7,4 +7,15 @@ localparam HBURST_WIDTH = 1;
 localparam DATA_WIDTH   = 32;
 localparam STRB_WIDTH   = DATA_WIDTH / 8;
 
+/// Cache memory parameters ///
+
+localparam SET_NUMBER   = 8;
+localparam WORDS_NUMBER = 4;
+
+localparam OFFSET_SIZE  = 2;
+localparam INDEX_SIZE   = $clog2( SET_NUMBER );
+localparam TAG_SIZE     = DATA_WIDTH - INDEX_SIZE - OFFSET_SIZE; 
+
+localparam HIT_ADDR_SIZE = $clog2( WORDS_NUMBER ); 
+
 endpackage
