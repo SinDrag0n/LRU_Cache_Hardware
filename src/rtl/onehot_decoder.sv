cb6e7a5f6be1e3eaf92726d002a002c90167e618
SinDrag0n <vladislavbelyaev04@gmail.com>
2025-10-11 22:51:55 +0300

    style(rtl): codestyle issues resolved

--- src/rtl/onehot_decoder.sv
+++ src/rtl/onehot_decoder.sv
@@ -11,15 +11,18 @@ module onehot_decoder (
 logic [WORDS_NUMBER - 1:0] lsb_zero;
 assign lsb_zero = ( data_i & ~( data_i + 1'b1 ) ) << 1'b1;  // shift to left to find onehot vector of first zero from most significant active bit
 
+logic [$clog2( WORDS_NUMBER ) - 1:0] data;
+
 always_comb begin
   data_o = $clog2( WORDS_NUMBER )'(0);
   for ( int i = 0; i < WORDS_NUMBER; i = i + 1 ) begin
     if ( lsb_zero[i] ) begin
-       data_o = i;
+       data = i;
     end
   end
 end
 
+assign data_o     = data;
 assign set_full_o = &data_i;
 // assign set_empty_o = ~|data_i;
 
